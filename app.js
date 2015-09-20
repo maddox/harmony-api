061e1349182bdf43de75a5032640ba7466eec2cf
Jon Maddox <jon@jonmaddox.com>
2015-09-19 22:20:20 -0400

    create mqtt client

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ var harmonyActivitiesCache = {}
 var harmonyActivityUpdateInterval = 1*60*1000 // 1 minute
 var harmonyActivityUpdateTimer
 
+var mqttClient = mqtt.connect(config.mqtt_host);
 var app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')));
