8ae451697b956e447110a245411292400fd7460d
Umer Farooq <umer.farooq@inin.com>
2016-11-28 13:00:20 -0500

    Fixed the index page.

--- app.js
+++ app.js
@@ -533,6 +533,7 @@ app.get('/hubs_for_index', function(req, res){
     output += '<h3 class="hub-name">' + hubSlug.replace('-', ' ') + '</h3>'
     output += '<p><span class="method">GET</span> <a href="/hubs/' + hubSlug + '/status">/hubs/' + hubSlug + '/status</a></p>'
     output += '<p><span class="method">GET</span> <a href="/hubs/' + hubSlug + '/activities">/hubs/' + hubSlug + '/activities</a></p>'
+    output += '<p><span class="method">GET</span> <a href="/hubs/' + hubSlug + '/commands">/hubs/' + hubSlug + '/commands</a></p>'
     cachedHarmonyActivities(hubSlug).forEach(function(activity) {
       path = '/hubs/' + hubSlug + '/activities/' + activity.slug + '/commands'
       output += '<p><span class="method">GET</span> <a href="' + path + '">' + path + '</a></p>'
